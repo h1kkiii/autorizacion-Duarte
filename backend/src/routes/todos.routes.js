94eeb5baf7fee227465d131a58b05ff17ffc9a16
unknown <zenagsuma5@gmail.com>
2024-11-27 10:11:41 -0300

    se añadieron las rutas para los todos

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import {
+  deleteTodosCtrl,
+  editTaskCtrl,
+  getAllTodosCtrl,
+  postTodosCtrl,
+  TodobyIdCtrl,
+} from "../controllers/todos.controllers.js";
+import validarJwt from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
-todosRouter.get("/", getAllTodosCtrl);
+todosRouter.get("/", validarJwt, getAllTodosCtrl);
+todosRouter.get("/:id", validarJwt, TodobyIdCtrl);
+todosRouter.delete("/:id", validarJwt, deleteTodosCtrl);
+todosRouter.put("/:id", validarJwt, editTaskCtrl);
+todosRouter.post("/", validarJwt, postTodosCtrl);
 
 export { todosRouter };
